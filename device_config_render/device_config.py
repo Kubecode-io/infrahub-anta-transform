563e240130038a4ff49c3c47f901dfa806b94cf2
Steve Corp <steve@kubecode.io>
2025-11-13 15:31:26 +0000

    Initial commit.

---
+++ device_config_render/device_config.py
@@ -0,0 +1,11 @@
+from infrahub_sdk.transforms import InfrahubTransform
+class DeviceConfigTransform(InfrahubTransform):
+    query = "device_config_query"
+    async def transform(self, data):
+        device = data["KubeDevice"]["edges"][0]["node"]
+        device_name = device["name"]["value"]
+        device_description = device["description"]["value"]
+        return {
+            "device_hostname": device_name,
+            "device_description": f"*{device_description}*"
+        }
