b36d37783a80ea9a74dd105f516e02f4a14882cb
Steve Corp <steve@kubecode.io>
2025-11-14 09:48:06 +0000

    switch check.

---
+++ switch_check/switch_check.py
@@ -0,0 +1,38 @@
+from infrahub_sdk.transforms import InfrahubTransform
+import json
+import requests
+import time
+class DeviceConfigTransform(InfrahubTransform):
+    query = "device_config_query"
+    async def transform(self, data):
+        device = data["AvdLeafswitch"]["edges"][0]["node"]
+        device_name = device["name"]["value"]
+        device_primary_address = device["primary_address"]["node"]["address"]["value"]
+
+        s = requests.Session()
+        kriten_url = "http://kriten-dev.192.168.10.190.nip.io/api/v1/jobs/"
+        kriten_launch_url = kriten_url + "hello-kriten"
+
+        payload = json.dumps({
+        "device_name": device_name,
+        "device_primary_address": device_primary_address
+        })
+        headers = {
+        'Content-Type': 'application/json',
+        'Token': 'kri_Rnx8Pvr2jBalEjvfwA4nIHUhI8lIUGHIK5zH'
+        }
+        url = kriten_launch_url
+        response = s.post(url, headers=headers, data=payload)
+
+        job_id = response.json()['id']
+        url = kriten_url + job_id
+        print("job_id:", job_id)
+        completed = 0
+        while completed == 0:
+            time.sleep(5)
+            response = s.get(url, headers=headers)
+            result = response.json()
+            completed = result['completed']
+            print("completed:", completed)
+        print(response.json()['json_data'])
+        return response.json()['json_data']
